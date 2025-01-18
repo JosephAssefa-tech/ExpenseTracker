55b0102eafd08ad31175f2fe745c9bb6ca6c7cd6
JosephAssefa-tech <yosephassefa85@gmail.com>
2025-01-18 01:35:42 -0800

    adding budget and notification entity

---
+++ ExpenseTrackerDomainLayer/Entities/Budget.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseTrackerDomainLayer.Entities
+{
+    public class Budget
+    {
+        [Key]
+        public Guid BudgetId { get; set; }
+        public Guid UserId { get; set; }
+        public Guid? CategoryId { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public bool IsRecurring { get; set; }
+
+    }
+}
